cd5b980bed3aa3cb8642657bc631dac3c24bea70
Shoury Sharma <shanuu12e@gmail.com>
2023-01-29 23:18:17 +0530

    feat: integrated auth middleware

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -1,9 +1,54 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"example/go-auth/responses"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
 
 func JWTMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		if c.Request.Header["Token"] != nil {
+			bearerToken := c.Request.Header.Get("Token")
+			_, err := jwt.Parse(bearerToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+					c.JSON(
+						http.StatusUnauthorized,
+						responses.Response{
+							Status:  http.StatusUnauthorized,
+							Message: "error",
+							Data:    map[string]interface{}{"data": "You are unauthorized"},
+						},
+					)
+				}
+				return "", nil
+			})
 
+			if err != nil {
+				c.JSON(
+					http.StatusUnauthorized,
+					responses.Response{
+						Status:  http.StatusUnauthorized,
+						Message: "error",
+						Data:    map[string]interface{}{"data": "You are unauthorized"},
+					},
+				)
+				return
+			}
+
+			c.Next()
+
+		} else {
+			c.JSON(
+				http.StatusUnauthorized,
+				responses.Response{
+					Status:  http.StatusUnauthorized,
+					Message: "error",
+					Data:    map[string]interface{}{"data": "You are unauthorized"},
+				},
+			)
+		}
 	}
 }
--- routes/user_route.go
+++ routes/user_route.go
@@ -2,10 +2,15 @@ package routes
 
 import (
 	"example/go-auth/controllers"
+	"example/go-auth/middlewares"
+
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoute(router *gin.Engine) {
+
+	router.Use(middlewares.JWTMiddleware())
+	
 	router.POST("/user", controllers.CreateUser())
 	router.GET("/users", controllers.GetAllUsers())
 	router.DELETE("/user/:userId", controllers.DeleteUser())
